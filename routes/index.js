787e64f312eb680a2a929b699fecff9c46d9f013
Brooks Becton <broabect@ut.utm.edu>
2018-01-02 22:53:42 -0600

    Added Sign In

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index');
+router.get("/", function(req, res, next) {
+  res.render("index");
+});
+
+/* GET home page. */
+router.get("/signIn", function(req, res, next) {
+  res.render("signIn");
 });
 
 module.exports = router;
